2050dcc9d217d721fb443b6247a2c7c61557a7a3
Gummetje <rubenlavooij@Rubens-Mac.local>
2013-10-21 21:13:14 +0200

    s

--- index.php
+++ index.php
@@ -1,104 +1,256 @@
-<?php
-	$str_browser_language = !empty($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? strtok(strip_tags($_SERVER['HTTP_ACCEPT_LANGUAGE']), ',') : '';
-	$str_browser_language = !empty($_GET['language']) ? $_GET['language'] : $str_browser_language;
-	switch (substr($str_browser_language, 0,2))
-	{
-		case 'de':
-			$str_language = 'de';
-			break;
-		case 'en':
-			$str_language = 'en';
-			break;
-		default:
-			$str_language = 'en';
-	}
+<div class="header">
+	<h1>Gradient Buttons</h1>
+</div>
+<div class="content">
+  
+    	<h2>Colors</h2>
+		<ul class="color">
+			<li class="green"></li>
+			<li class="blue"></li>
+			<li class="red"></li>			
+			<li class="purple"></li>
+			<li class="cyan"></li>
+			<li class="yellow"></li>
+		</ul>
+	<div class="container">
+		<h2>mini</h2>
+		<a href="#" class="btn-gradient cyan mini">Button</a>
+		<a href="#" class="btn-gradient orange mini">Button</a>
+		<a href="#" class="btn-gradient blue mini">Button</a>
+		<a href="#" class="btn-gradient purple mini">Button</a>
+		<a href="#" class="btn-gradient green mini">Button</a>
+		<a href="#" class="btn-gradient yellow mini">Button</a>
+		<a href="#" class="btn-gradient red mini">Button</a>
 
-	$arr_available_languages = array();
-	$arr_available_languages[] = array('str_name' => 'English', 'str_token' => 'en');
-	$arr_available_languages[] = array('str_name' => 'Deutsch', 'str_token' => 'de');
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-gradient red mini"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+	<div class="container">
+		<h2>small</h2>
+		<a href="#" class="btn-gradient cyan small">Button</a>
+		<a href="#" class="btn-gradient orange small">Button</a>
+		<a href="#" class="btn-gradient blue small">Button</a>
+		<a href="#" class="btn-gradient purple small">Button</a>
+		<a href="#" class="btn-gradient green small">Button</a>
+		<a href="#" class="btn-gradient yellow small">Button</a>
+		<a href="#" class="btn-gradient red small">Button</a>
 
-	$str_available_languages = (string) '';
-	foreach ($arr_available_languages as $arr_language)
-	{
-		if ($arr_language['str_token'] !== $str_language)
-		{
-			$str_available_languages .= '<a href="'.strip_tags($_SERVER['PHP_SELF']).'?language='.$arr_language['str_token'].'" lang="'.$arr_language['str_token'].'" xml:lang="'.$arr_language['str_token'].'" hreflang="'.$arr_language['str_token'].'">'.$arr_language['str_name'].'</a> | ';
-		}
-	}
-	$str_available_languages = substr($str_available_languages, 0, -3);
-?>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-gradient red small"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+	<div class="container">
+		<h2>normal</h2>
+		<a href="#" class="btn-gradient cyan">Button</a>
+		<a href="#" class="btn-gradient orange">Button</a>
+		<a href="#" class="btn-gradient blue">Button</a>
+		<a href="#" class="btn-gradient purple">Button</a>
+		<a href="#" class="btn-gradient green">Button</a>
+		<a href="#" class="btn-gradient yellow">Button</a>
+		<a href="#" class="btn-gradient red">Button</a>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head lang="<?php echo $str_language; ?>" xml:lang="<?php echo $str_language; ?>">
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title>MAMP PRO - manage your websites locally</title>
-	<style type="text/css">
-		body {
-			font-family: Arial, Helvetica, sans-serif;
-			font-size: .9em;
-			color: #000000;
-			background-color: #FFFFFF;
-			margin: 0;
-			padding: 10px 20px 20px 20px;
-		}
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-gradient red"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+	<div class="container">
+		<h2>large</h2>
+		<a href="#" class="btn-gradient cyan large">Button</a>
+		<a href="#" class="btn-gradient orange large">Button</a>
+		<a href="#" class="btn-gradient blue large">Button</a>
+		<a href="#" class="btn-gradient purple large">Button</a>
+		<a href="#" class="btn-gradient green large">Button</a>
+		<a href="#" class="btn-gradient yellow large">Button</a>
+		<a href="#" class="btn-gradient red large">Button</a>
 
-		samp {
-			font-size: 1.3em;
-		}
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-gradient red large"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+	<div class="container">
+		<h2>block</h2>
+		<a href="#" class="btn-gradient cyan block">Block Button</a>
+		<a href="#" class="btn-gradient orange block">Block Button</a>
+		<a href="#" class="btn-gradient blue block">Block Button</a>
+		<a href="#" class="btn-gradient purple block">Block Button</a>
+		<a href="#" class="btn-gradient green block">Block Button</a>
+		<a href="#" class="btn-gradient yellow block">Block Button</a>
+		<a href="#" class="btn-gradient red block">Block Button</a>
 
-		a {
-			color: #000000;
-			background-color: #FFFFFF;
-		}
-		
-		sup a {
-			text-decoration: none;
-		}
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-gradient red block"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+</div>
 
-		hr {
-			margin-left: 90px;
-		    height: 1px;
-		    color: #000000;
-			background-color: #000000;
-		    border: none;
-		}
 
-		#logo {
-			margin-bottom: 10px;
-		}
+<div class="header">
+	<h1>.btn-two</h1>
+</div>
+<div class="content">
+<section>
+	<div class="container">
+		<h2>mini</h2>
+		<a href="#" class="btn-two green mini">Button</a>
+		<a href="#" class="btn-two blue mini">Button</a>
+		<a href="#" class="btn-two red mini">Button</a>
+		<a href="#" class="btn-two purple mini">Button</a>
+		<a href="#" class="btn-two cyan mini">Button</a>
+		<a href="#" class="btn-two yellow mini">Button</a>
 
-		.text {
-			width: 80%;
-			margin-left: 90px;
-			line-height: 140%;
-		}
-	</style>
-</head>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-two green mini"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+	<div class="container">
+		<h2>small</h2>
+		<a href="#" class="btn-two green small">Button</a>
+		<a href="#" class="btn-two blue small">Button</a>
+		<a href="#" class="btn-two red small">Button</a>
+		<a href="#" class="btn-two purple small">Button</a>
+		<a href="#" class="btn-two cyan small">Button</a>
+		<a href="#" class="btn-two yellow small">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-two green small"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+	<div class="container">
+		<h2>normal</h2>
+		<a href="#" class="btn-two green">Button</a>
+		<a href="#" class="btn-two blue">Button</a>
+		<a href="#" class="btn-two red">Button</a>
+		<a href="#" class="btn-two purple">Button</a>
+		<a href="#" class="btn-two cyan">Button</a>
+		<a href="#" class="btn-two yellow">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-two green"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+	<div class="container">
+		<h2>large</h2>
+		<a href="#" class="btn-two green large">Button</a>
+		<a href="#" class="btn-two blue large">Button</a>
+		<a href="#" class="btn-two red large">Button</a>
+		<a href="#" class="btn-two purple large">Button</a>
+		<a href="#" class="btn-two cyan large">Button</a>
+		<a href="#" class="btn-two yellow large">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-two green large"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
 
-<body>
-	<p><img src="MAMP-PRO-Logo.gif" id="logo" alt="MAMP PRO - manage your websites locally" width="250" height="76" /></p>
-	<?php if ($str_language == 'de'): ?>
-		<p class="text"><strong>Der virtuelle <span lang="en" xml:lang="en">Host</span> wurde erfolgreich eingerichtet.</strong></p>
-		<p class="text">Wenn Sie diese Seite sehen, dann bedeutet dies, dass der neue virtuelle <span lang="en" xml:lang="en">Host</span> erfolgreich eingerichtet wurde. Jetzt muss noch der richtige <span lang="en" xml:lang="en">Web</span>-Inhalt hinzugefügt und diese Platzhalter-Seite<sup><a href="#footnote_1">1</a></sup> ersetzt <abbr title="beziehungsweise">bzw.</abbr> gelöscht werden.</p>
-		<p class="text">
-			Server-Name: <samp><?php echo $_SERVER['SERVER_NAME']; ?></samp><br />
-			Document-Root: <samp><?php echo $_SERVER['DOCUMENT_ROOT']; ?></samp>
-		</p>
-		<p class="text" id="footnote_1"><small><sup>1</sup> Dateien: <samp>index.php</samp> und <samp>MAMP-PRO-Logo.gif</samp></small></p>
-		<hr />
-		<p class="text">Diese Seite in: <?php echo $str_available_languages; ?></p>
-	<?php elseif ($str_language == 'en'): ?>
-		<p class="text"><strong>The virtual host was set up successfully.</strong></p>
-		<p class="text">If you can see this page, your new virtual host was set up successfully. Now, web contents can be added and this placeholder page<sup><a href="#footnote_1">1</a></sup> should be replaced or deleted.</p>
-		<p class="text">
-			Server name: <samp><?php echo $_SERVER['SERVER_NAME']; ?></samp><br />
-			Document-Root: <samp><?php echo $_SERVER['DOCUMENT_ROOT']; ?></samp>
-		</p>
-		<p class="text" id="footnote_1"><small><sup>1</sup> Files: <samp>index.php</samp> and <samp>MAMP-PRO-Logo.gif</samp></small></p>
-		<hr />
-		<p class="text">This page in: <?php echo $str_available_languages; ?></p>
-	<?php endif; ?>
-</body>
-</html>
+	<div class="container">
+		<h2>rounded</h2>
+		<a href="#" class="btn-two green rounded">Button</a>
+		<a href="#" class="btn-two blue rounded">Button</a>
+		<a href="#" class="btn-two red rounded">Button</a>
+		<a href="#" class="btn-two purple rounded">Button</a>
+		<a href="#" class="btn-two cyan rounded">Button</a>
+		<a href="#" class="btn-two yellow rounded">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-two green rounded"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+	<div class="container">
+		<h2>block</h2>
+		<a href="#" class="btn-two green block">Button</a>
+		<a href="#" class="btn-two blue block">Button</a>
+		<a href="#" class="btn-two red block">Button</a>
+		<a href="#" class="btn-two purple block">Button</a>
+		<a href="#" class="btn-two cyan block">Button</a>
+		<a href="#" class="btn-two yellow block">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-two green block"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+</section>
+</div>
+
+<div class="header">
+	<h1>Default Buttons</h1>
+</div>
+<div class="content">
+	<section>
+
+	<div class="container">
+		<h2>mini</h2>
+		<a href="#" class="btn green mini">Button</a>
+		<a href="#" class="btn blue mini">Button</a>
+		<a href="#" class="btn red mini">Button</a>
+		<a href="#" class="btn purple mini">Button</a>
+		<a href="#" class="btn cyan mini">Button</a>
+		<a href="#" class="btn yellow mini">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn green mini"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+
+	<div class="container">
+		<h2>small</h2>
+		<a href="#" class="btn green small">Button</a>
+		<a href="#" class="btn blue small">Button</a>
+		<a href="#" class="btn red small">Button</a>
+		<a href="#" class="btn purple small">Button</a>
+		<a href="#" class="btn cyan small">Button</a>
+		<a href="#" class="btn yellow small">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn green small"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+
+	<div class="container">
+		<h2>normal</h2>
+		<a href="#" class="btn green">Button</a>
+		<a href="#" class="btn blue">Button</a>
+		<a href="#" class="btn red">Button</a>
+		<a href="#" class="btn purple">Button</a>
+		<a href="#" class="btn cyan">Button</a>
+		<a href="#" class="btn yellow">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn green"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+
+	<div class="container">
+		<h2>large</h2>
+		<a href="#" class="btn green large">Button</a>
+		<a href="#" class="btn blue large">Button</a>
+		<a href="#" class="btn red large">Button</a>
+		<a href="#" class="btn purple large">Button</a>
+		<a href="#" class="btn cyan large">Button</a>
+		<a href="#" class="btn yellow large">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn green large"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+
+	<div class="container">
+		<h2>rounded</h2>
+		<a href="#" class="btn green rounded">Button</a>
+		<a href="#" class="btn blue rounded">Button</a>
+		<a href="#" class="btn red rounded">Button</a>
+		<a href="#" class="btn purple rounded">Button</a>
+		<a href="#" class="btn cyan rounded">Button</a>
+		<a href="#" class="btn yellow rounded">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn green rounded"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+
+	<div class="container">
+		<h2>block</h2>
+		<a href="#" class="btn green block">Button</a>
+		<a href="#" class="btn blue block">Button</a>
+		<a href="#" class="btn red block">Button</a>
+		<a href="#" class="btn purple block">Button</a>
+		<a href="#" class="btn cyan block">Button</a>
+		<a href="#" class="btn yellow block">Button</a>
+		<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-3d green"</span>>Button&lt;/<span class="anc">a</span>></pre>
+	</div>
+
+	</section>
+</div>
+
+
+<div class="header">
+	<h1>3D Buttons</h1>
+</div>
+<div class="content-3d">
+
+<div class="btn-container">
+	<a href="#" class="btn-3d green">Button</a>
+	<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-3d green"</span>>Button&lt;/<span class="anc">a</span>></pre>
+</div>
+
+<div class="btn-container">
+	<a href="#" class="btn-3d blue">Button</a>
+	<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-3d blue"</span>>Button&lt;/<span class="anc">a</span>></pre>
+</div>
+
+<div class="btn-container">
+	<a href="#" class="btn-3d red">Button</a>
+	<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-3d red"</span>>Button&lt;/<span class="anc">a</span>></pre>
+</div>
+
+<div class="btn-container">
+	<a href="#" class="btn-3d purple">Button</a>
+	<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-3d purple"</span>>Button&lt;/<span class="anc">a</span>></pre>
+</div>
+<div class="btn-container">
+	<a href="#" class="btn-3d cyan">Button</a>
+	<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-3d cyan"</span>>Button&lt;/<span class="anc">a</span>></pre>
+</div>
+<div class="btn-container">
+	<a href="#" class="btn-3d yellow">Button</a>
+	<pre>&lt;<span class="anc">a</span> <span class="att">href</span>=<span class="val">"#"</span> <span class="att">class</span>=<span class="val">"btn-3d yellow"</span>>Button&lt;/<span class="anc">a</span>></pre>
+</div>
+</div>
